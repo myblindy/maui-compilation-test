2490cf668f54b028cad594b4e2b0d01cf707fb8b
Marius <tweety.04.01@gmail.com>
2024-12-05 16:15:27 -0500

    Add project files.

---
+++ maui-compilation-test/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.MSBuild;
+
+await CompileAssembly("""
+    using Microsoft.Maui.Controls;
+
+    public static class C
+    {
+        public static Element? Test() => null;
+    }
+    """);
+
+static async Task<bool> CompileAssembly(string code)
+{
+    MSBuildLocator.RegisterDefaults();
+
+    using var workspace = MSBuildWorkspace.Create(new Dictionary<string, string>
+    {
+        ["Configuration"] = "Debug",
+        ["Platform"] = "AnyCPU",
+        ["TargetFramework"] = "net9.0-windows10.0.19041.0"
+    });
+
+    var project = await workspace.OpenProjectAsync("IosPlayerMaui.CompiledPages.csproj");
+
+    var syntaxTree = CSharpSyntaxTree.ParseText(code);
+    project = project.AddDocument("IosPlayerMaui.CompiledPages.cs", await syntaxTree.GetTextAsync().ConfigureAwait(false)).Project;
+
+    if (await project.GetCompilationAsync().ConfigureAwait(false) is not { } compilation)
+        return false;
+
+    if (compilation.GetDiagnostics() is [_, ..] diagnostics)
+    {
+        foreach (var diagnostic in diagnostics)
+            Console.Error.WriteLine(diagnostic);
+        return false;
+    }
+
+    var emitResult = compilation.Emit("IosPlayerMaui.CompiledPages.dll");
+    return true;
+}
